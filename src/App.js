be77bb3f8b3cbf85a2200f76312974f16a6ffa2f
Divyansh Vishwakarma <dvish006@gmail.com>
2024-07-20 15:33:52 +0530

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import React, { useState } from 'react';
 import BlogList from './components/BlogList';
 import PostDetails from './components/PostDetails';
+import Navbar from './components/Navbar';
+
 
 const App = () => {
   const [selectedPost, setSelectedPost] = useState(null);
 
   return (
     <div>
+
+      <Navbar/>
       {selectedPost ? (
         <PostDetails post={selectedPost} onBack={() => setSelectedPost(null)} />
       ) : (
